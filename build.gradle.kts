3314b3f7bab5d037855a270693faf191551eb895
jwstegemann <jwstegemann@gmail.com>
2022-11-25 16:35:17 +0100

    set fritz-version to 1.0-RC2

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,17 +1,16 @@
 plugins {
-    kotlin("multiplatform") version "1.7.10"
-
+    kotlin("multiplatform") version "1.7.20"
     // KSP support
-    id("com.google.devtools.ksp") version "1.7.10-1.0.6"
+    id("com.google.devtools.ksp") version "1.7.20-1.0.6"
 }
 
+
 repositories {
-    mavenLocal()
     mavenCentral()
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots/") // new repository here
 }
 
-val fritz2Version = "1.0-SNAPSHOT"
+val fritz2Version = "1.0-RC2"
 
 //group = "my.fritz2.app"
 //version = "0.0.1-SNAPSHOT"
@@ -36,16 +35,16 @@ kotlin {
         val jsMain by getting {
             dependencies {
                 // tailwind
-                implementation(npm("tailwindcss", "3.1.2"))
-                // implementation(npm("@tailwindcss/forms", "0.5.2")) // optional
+                implementation(npm("tailwindcss", "3.2.1"))
+                // implementation(npm("@tailwindcss/forms", "0.5.3")) // optional
 
                 // webpack
-                implementation(devNpm("postcss", "8.4.14"))
-                implementation(devNpm("postcss-loader", "7.0.0"))
-                implementation(devNpm("autoprefixer", "10.4.7"))
+                implementation(devNpm("postcss", "8.4.17"))
+                implementation(devNpm("postcss-loader", "7.0.1"))
+                implementation(devNpm("autoprefixer", "10.4.12"))
                 implementation(devNpm("css-loader", "6.7.1"))
                 implementation(devNpm("style-loader", "3.3.1"))
-                implementation(devNpm("cssnano", "5.1.11"))
+                implementation(devNpm("cssnano", "5.1.13"))
             }
         }
     }
@@ -58,12 +57,6 @@ dependencies {
     add("kspCommonMainMetadata", "dev.fritz2:lenses-annotation-processor:$fritz2Version")
 }
 kotlin.sourceSets.commonMain { kotlin.srcDir("build/generated/ksp/metadata/commonMain/kotlin") }
-
-// Fixes webpack-cli incompatibility by pinning the newest version.
-// https://youtrack.jetbrains.com/issue/KTIJ-22030
-rootProject.extensions.configure<org.jetbrains.kotlin.gradle.targets.js.nodejs.NodeJsRootExtension> {
-    versions.webpackCli.version = "4.10.0"
-}
 
 tasks.withType<org.jetbrains.kotlin.gradle.dsl.KotlinCompile<*>>().all {
     if (name != "kspCommonMainKotlinMetadata") dependsOn("kspCommonMainKotlinMetadata")
